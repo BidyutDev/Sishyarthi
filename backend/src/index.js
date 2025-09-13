3cedbcc1fef963ce54084121b70d59facb667c07
AnirbanNath-dev <anirbaniitian@gmail.com>
2025-09-14 01:52:02 +0530

    Setup backend : 🌟ft. Anirban

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,15 @@
 import express from "express"
+import cors from "cors"
+import router from "./routes"
+import { PORT } from "./config"
+
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use("/api/v1" , router)
 
-
-app.listen()
+app.listen(PORT , ()=> {
+    console.log(`Server is running on ${PORT}`)
+})
