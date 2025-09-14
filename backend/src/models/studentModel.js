aa679b6dba157f289df77258611d5c054168f77c
AnirbanNath-dev <anirbaniitian@gmail.com>
2025-09-14 20:15:41 +0530

    Add student model : 🌟ft. Anirban

---
+++ backend/src/models/studentModel.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  personalDetails: {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    middleName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      enum: ["obc", "sc", "st", "general", "other"],
+      required: true,
+      trim: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+  },
+  familyDetails: {
+    fatherName: { 
+      type: String, 
+      trim: true, 
+      required: true 
+    },
+    motherName: { 
+      type: String, 
+      trim: true, 
+      required: true 
+    },
+    fatherOccupation: { 
+      type: String, 
+      trim: true 
+    },
+    motherOccupation: { 
+      type: String, 
+      trim: true 
+    },
+    siblings: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    familyIncome : {
+        type : Number,
+        required : true,
+    }
+  }
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+export default Student;
