c52f65d21f025bbaae4381853847080ba79a5fd3
AnirbanNath-dev <anirbaniitian@gmail.com>
2025-09-14 20:29:36 +0530

    Add moderator model : 🌟ft. Anirban

---
+++ backend/src/models/moderatorModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const moderatorSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Moderator = mongoose.model('Moderator', moderatorSchema);
+
+export default Moderator;
